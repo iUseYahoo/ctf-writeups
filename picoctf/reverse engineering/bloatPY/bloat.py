a3523f509531ff4ed688a318915a1f8562df8e88
Nicholas <64442818+iUseYahoo@users.noreply.github.com>
2022-09-04 15:56:19 +1000

    Nicer code prints

--- picoctf/reverse engineering/bloatPY/bloat.py
+++ picoctf/reverse engineering/bloatPY/bloat.py
@@ -14,32 +14,27 @@ def arg133(arg432):
   So i change it to if it does not (!=) equal the required parameter (a[71]+a[64]+a[79]+a[79]+a[88]+a[66]+a[71]+a[64]+a[77]+a[66]+a[68])
   From here we get the picoCTF flag: 
   """
-    print("-----------------------------------\narg133")
-    if arg432 != a[71]+a[64]+a[79]+a[79]+a[88]+a[66]+a[71]+a[64]+a[77]+a[66]+a[68]:
-        return True
-    else:
-        print(a[51]+a[71]+a[64]+a[83]+a[94]+a[79]+a[64]+a[82]+a[82]+a[86]+a[78]+\
-a[81]+a[67]+a[94]+a[72]+a[82]+a[94]+a[72]+a[77]+a[66]+a[78]+a[81]+\
-a[81]+a[68]+a[66]+a[83])
-    sys.exit(0)
-    print("def arg123 else hit!\n------------------------------------------")
-    return False
+  print("-----------------------------------\narg133\n-----------------------------------")
+  if arg432 != a[71]+a[64]+a[79]+a[79]+a[88]+a[66]+a[71]+a[64]+a[77]+a[66]+a[68]:
+      return True
+  else:
+      print(a[51]+a[71]+a[64]+a[83]+a[94]+a[79]+a[64]+a[82]+a[82]+a[86]+a[78]+a[81]+a[67]+a[94]+a[72]+a[82]+a[94]+a[72]+a[77]+a[66]+a[78]+a[81]+a[81]+a[68]+a[66]+a[83])
+      sys.exit(0)
+      print("----------------------------------\ndef arg123 else hit!\n-----------------------------------")
+      return False
 
 
 def arg111(arg444):
   # print for figuring out where the script is at when its running
     print("\n-------------------------\narg111 is this\n")
-    return "\n\nYour PicoCTF flag is : " + arg122(arg444.decode(), a[81]+a[64]+a[79]+a[82]+a[66]+a[64]+a[75]+\
-a[75]+a[72]+a[78]+a[77])
-    print("\n\n-----------------------------------\n")
+    return "\n\n--------------------- picoCTF Flag --------------------\nYour PicoCTF flag is : " + arg122(arg444.decode(), a[81]+a[64]+a[79]+a[82]+a[66]+a[64]+a[75]+\
+a[75]+a[72]+a[78]+a[77]) + "\n----------------------------------------------------------------"
 
 def arg232():
   # print for figuring out where the script is at when its running
     print("\n-----------------------------arg232 is this!\n")
-    return input(a[47]+a[75]+a[68]+a[64]+a[82]+a[68]+a[94]+a[68]+a[77]+a[83]+\
-a[68]+a[81]+a[94]+a[66]+a[78]+a[81]+a[81]+a[68]+a[66]+a[83]+\
-a[94]+a[79]+a[64]+a[82]+a[82]+a[86]+a[78]+a[81]+a[67]+a[94]+\
-a[69]+a[78]+a[81]+a[94]+a[69]+a[75]+a[64]+a[70]+a[25]+a[94])
+    # comment out the input so it shoudl skip right?
+    #return input(a[47]+a[75]+a[68]+a[64]+a[82]+a[68]+a[94]+a[68]+a[77]+a[83]+a[68]+a[81]+a[94]+a[66]+a[78]+a[81]+a[81]+a[68]+a[66]+a[83]+a[94]+a[79]+a[64]+a[82]+a[82]+a[86]+a[78]+a[81]+a[67]+a[94]+a[69]+a[78]+a[81]+a[94]+a[69]+a[75]+a[64]+a[70]+a[25]+a[94])
     print("\n------------------------------------------")
 
 def arg132():
@@ -52,15 +47,13 @@ def arg132():
 def arg112():
   # print for figuring out where the script is at when its running
     print("\n---------------------------------------------------\narg112 was hit!")
-    print(a[54]+a[68]+a[75]+a[66]+a[78]+a[76]+a[68]+a[94]+a[65]+a[64]+a[66]+\
-a[74]+a[13]+a[13]+a[13]+a[94]+a[88]+a[78]+a[84]+a[81]+a[94]+a[69]+\
-a[75]+a[64]+a[70]+a[11]+a[94]+a[84]+a[82]+a[68]+a[81]+a[25])
+    print(a[54]+a[68]+a[75]+a[66]+a[78]+a[76]+a[68]+a[94]+a[65]+a[64]+a[66]+a[74]+a[13]+a[13]+a[13]+a[94]+a[88]+a[78]+a[84]+a[81]+a[94]+a[69]+a[75]+a[64]+a[70]+a[11]+a[94]+a[84]+a[82]+a[68]+a[81]+a[25])
     print("\n------------------------------------------------------------------")
 
 def arg122(arg432, arg423):
   # print for figuring out where the script is at when its running
     print("\n-------------------------------------------------------------")
-    print("arg122 was hit!\n-------------------------------------------------------------")
+    print("\narg122 was hit!\n-------------------------------------------------------------")
     arg433 = arg423
     i = 0
     while len(arg433) < len(arg432):
@@ -77,5 +70,5 @@ arg432 = arg232()
 arg133(arg432)
 arg112()
 arg423 = arg111(arg444)
-print(arg423 + "this is arg423() aka arg232()")
+print("\n" + arg423 + "\n\nthis is arg423() aka arg232()")
 sys.exit(0)
